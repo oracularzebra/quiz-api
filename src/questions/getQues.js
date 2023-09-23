0dcf3ea8d7570f6f0221c034a2794f8935e33aad
onion <onion@orange.com>
2023-09-24 00:51:55 +0530

    Using random to get random questions and made different tables for storing questions and correct options

--- src/questions/getQues.js
+++ src/questions/getQues.js
@@ -2,16 +2,19 @@ const pool = require("../pg");
 
 async function getQues(quesCat, quesType, quesDifficulty, noOfQues=10){
     
-    const res = await pool.query(`select * from questions TABLESAMPLE BERNOULLI (20)    
-        where category='${quesCat}' 
+    console.log(quesCat, quesType, quesDifficulty);
+    const res = await pool.query(`select id,question,options from questions 
+        where sub_category='${quesCat}' 
         and type='${quesType}'
         and difficulty='${quesDifficulty}'
+        order by random()
         limit ${noOfQues};
         `);
     
-    console.log(res.rows.length)
+    console.log(res.rows)
     if(res.rows.length > 0) return [true, res.rows];
 
-    return [false, res]
+    return [false, []]
 }
+// getQues('Music', 'multiple', 'easy');
 module.exports = getQues;
