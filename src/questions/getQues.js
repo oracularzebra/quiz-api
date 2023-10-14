84b18431ff6432d659b7da8197292e5379fafa6e
onion <onion@orange.com>
2023-10-14 17:22:55 +0000

    Added db questiong fetch duplicate tries limit

--- src/questions/getQues.js
+++ src/questions/getQues.js
@@ -4,6 +4,7 @@ async function getQues(quesCat, quesType, quesDifficulty, noOfQues=10){
     
     let questions = [];
     let fetched_ids = [];
+    let tries_limit = noOfQues;
     for(let i=0;i<noOfQues;){
         const res = await pool.query(`select id,question,options from questions 
         where sub_category='${quesCat}' 
@@ -17,6 +18,7 @@ async function getQues(quesCat, quesType, quesDifficulty, noOfQues=10){
            questions.push(...res.rows);
            i++;
         }
+        else if(tries_limit-- == 0) break;
     }
     if(questions.length > 0) return [true, questions];
 
